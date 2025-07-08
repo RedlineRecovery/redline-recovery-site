90f4b31d0d6e4ea6faab460c1eb40bc937a673ad
RedlineRecovery <redline.recovery.fire@gmail.com>
2025-07-08 14:36:45 -0600

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+    <div style={{
+      color: "white",
+      background: "black",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+      padding: "20px"
+    }}>
+      <h1>BUILT ON BROKEN BONES AND HARD LESSONS.</h1>
+    </div>
+  );
+}
